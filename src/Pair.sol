c1691f6b05fd13aea72fa88abbd661e285275a74
Yves Boutellier <63062257+yvesbou@users.noreply.github.com>
2025-01-09 07:30:13 +0100

    important reentrancy protection forgotten

--- src/Pair.sol
+++ src/Pair.sol
@@ -275,6 +275,7 @@ contract Pair is ReentrancyGuard, ERC20, IERC3156FlashLender {
 
     function flashLoan(IERC3156FlashBorrower receiver, address token, uint256 amount, bytes calldata data)
         external
+        nonReentrant
         returns (bool)
     {
         if (amount > maxFlashLoan(token)) revert FlashloanAboveMaxAmount();
